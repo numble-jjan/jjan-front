161084c7905ceb20fd4135f1c94e610ce361718e
Choi Hong Gyu <zxolqs@gmail.com>
2022-11-11 19:57:36 +0900

    [#36] feat :  Input 컴포넌트 위치 변경
    - Input 컴포넌트 @shared/components 폴더에서 @/components로 변경

---
+++ src/@components/Input/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { InputHTMLAttributes } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import styled from 'styled-components';
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+  register: UseFormRegisterReturn;
+  errorMessage?: string | null;
+  required: boolean;
+}
+
+const Input = ({
+  register,
+  errorMessage,
+  required,
+  placeholder,
+  type,
+}: IProps) => (
+  <>
+    <InputWrapper isvalid={!!errorMessage}>
+      <StyledInput
+        placeholder={placeholder}
+        type={type}
+        required={required}
+        {...register}
+      />
+    </InputWrapper>
+    {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+  </>
+);
+
+const StyledInput = styled.input``;
+
+const InputWrapper = styled.div<{ isvalid?: boolean }>``;
+
+const ErrorMessage = styled.span``;
+
+export default Input;
