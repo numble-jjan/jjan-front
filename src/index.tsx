d91cbe8d4d5d4e50dc8f251b3b09cebca1967cc2
JZU0 <ljzh1030@gmail.com>
2022-11-13 22:28:57 +0900

    [#10] style : 전역 사이즈 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
-
 import App from './App';
-
+import styled from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import GlobalFonts from './styles/GlobalFonts';
+
+const IndexPageWrapper = styled.div`
+  width: 320px;
+  height: 658px;
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <GlobalStyle />
-      <GlobalFonts />
-      <App />
+      <IndexPageWrapper>
+        <GlobalStyle />
+        <GlobalFonts />
+        <App />
+      </IndexPageWrapper>
     </RecoilRoot>
   </React.StrictMode>,
 );
