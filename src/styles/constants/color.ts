763e8ffad34ab46c8543334af981aff2f14cb569
JZU0 <ljzh1030@gmail.com>
2022-11-14 11:44:12 +0900

    [#56] style : color 코드 설정

---
+++ src/styles/constants/color.ts
@@ -0,0 +1,16 @@
+const color = {
+  /*메인 보라 색상*/
+  main: '#683CED',
+  /*주황색 경고 문구 색상*/
+  system_warning: '#F58229',
+  /*인풋 박스 테두리 색상*/
+  input_box: '#DDDDDD',
+  placeholder: '#999999',
+  light_gray: '#EEEEEE',
+  light_gray2: '#F9F9F9',
+  gray: '#666666',
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+export default color;
