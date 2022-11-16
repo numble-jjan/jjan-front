bf8cae337a2a2d50bca362df3c2de1a94d4c9916
JZU0 <ljzh1030@gmail.com>
2022-11-17 02:31:09 +0900

    [#60] feat : onboarding page UI

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/main';
+import Onboarding from './pages/onboarding';
 
 const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/main" element={<Main />} />
+          <Route path="/onboarding" element={<Onboarding />} />
         </Routes>
       </BrowserRouter>
     </>
