609217fe20afa3d939b0278d3e4e2c57890eee89
JZU0 <ljzh1030@gmail.com>
2022-11-17 15:33:31 +0900

    [#70] feat: title bar 컴포넌트 수정

--- src/@components/TitleBar/index.tsx
+++ src/@components/TitleBar/index.tsx
@@ -2,21 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 type Props = {
-  children: any;
-  style?: any;
+  children: string;
+  type?: 'default' | 'essential';
 };
 
-const Title = styled.div`
-  color: black;
-  font-size: 16px;
-  font-weight: 500;
+const Root = styled.div`
   width: 90%;
   padding-bottom: 10px;
   border-bottom: 1px solid black;
 `;
 
-const TitleBar: React.FC<Props> = ({ children, style }) => {
-  return <Title style={style}>{children}</Title>;
+const Title = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+  display: inline-block;
+`;
+
+const Essential = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  margin-top: 2px;
+  display: inline-block;
+  float: right;
+`;
+
+const TitleBar: React.FC<Props> = ({ children, type }) => {
+  return type === 'essential' ? (
+    <Root>
+      <Title>{children}</Title>
+      <Essential>필수사항</Essential>
+    </Root>
+  ) : (
+    <Root>
+      <Title>{children}</Title>
+    </Root>
+  );
 };
 
 export default TitleBar;
