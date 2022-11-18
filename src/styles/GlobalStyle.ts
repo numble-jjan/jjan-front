aa3470c0beb05cb114b13367e2be5c3593aa955b
Choi Hong Gyu <zxolqs@gmail.com>
2022-11-19 01:30:07 +0900

    [#119] fix : font 미적용 문제 해결
    - font.d.ts 파일 생성 후 font 확장자 module로 정의
    - 이후 모듈로 폰트 import하여 사용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
+import NotoSans_KR_Bold_O from '../assets/fonts/NotoSansKR-Bold.otf';
+import NotoSans_KR_Bold_W from '../assets/fonts/NotoSansKR-Bold.woff';
+import NotoSans_KR_Bold_W2 from '../assets/fonts/NotoSansKR-Bold.woff2';
+import NotoSansKR_Light_O from '../assets/fonts/NotoSansKR-Light.otf';
+import NotoSansKR_Light_W from '../assets/fonts/NotoSansKR-Light.woff';
+import NotoSansKR_Light_W2 from '../assets/fonts/NotoSansKR-Light.woff2';
+import NotoSansKR_Medium_O from '../assets/fonts/NotoSansKR-Medium.otf';
+import NotoSansKR_Medium_W from '../assets/fonts/NotoSansKR-Medium.woff';
+import NotoSansKR_Medium_W2 from '../assets/fonts/NotoSansKR-Medium.woff2';
+import NotoSansKR_Regular_O from '../assets/fonts/NotoSansKR-Regular.otf';
+import NotoSansKR_Regular_W from '../assets/fonts/NotoSansKR-Regular.woff';
+import NotoSansKR_Regular_W2 from '../assets/fonts/NotoSansKR-Regular.woff2';
+
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
@@ -8,36 +21,36 @@ const GlobalStyle = createGlobalStyle`
   font-family: 'Noto Sans KR';
   font-style: normal;
   font-weight: 100;
-  src: url('../assets/fonts/NotoSansKR-Light.woff2') format('woff2'),
-    url('../assets/fonts/NotoSansKR-Light.woff') format('woff'),
-    url('../assets/fonts/NotoSansKR-Light.otf') format('truetype');
+  src: url(${NotoSansKR_Light_W2}) format('woff2'),
+    url(${NotoSansKR_Light_W}) format('woff'),
+    url(${NotoSansKR_Light_O}) format('truetype');
   }
 
   @font-face {
     font-family: 'Noto Sans KR';
     font-style: normal;
     font-weight: normal;
-    src: url('./fonts/NotoSansKR-Regular.woff2') format('woff2'),
-      url('../assets/fonts/NotoSansKR-Regular.woff') format('woff'),
-      url('../assets/fonts/NotoSansKR-Regular.otf') format('truetype');
+    src: url(${NotoSansKR_Regular_W2}) format('woff2'),
+      url(${NotoSansKR_Regular_W}) format('woff'),
+      url(${NotoSansKR_Regular_O}) format('truetype');
   }
 
   @font-face {
     font-family: 'Noto Sans KR';
     font-style: normal;
     font-weight: 500;
-    src: url('./fonts/NotoSansKR-Medium.woff2') format('woff2'),
-      url('./fonts/NotoSansKR-Medium.woff') format('woff'),
-      url('./fonts/NotoSansKR-Medium.otf') format('truetype');
+    src: url(${NotoSansKR_Medium_W2}) format('woff2'),
+      url(${NotoSansKR_Medium_W}) format('woff'),
+      url(${NotoSansKR_Medium_O}) format('truetype');
   }
 
   @font-face {
     font-family: 'Noto Sans KR';
     font-style: normal;
     font-weight: bold;
-    src: url('../assets/fonts/NotoSansKR-Bold.woff2') format('woff2'),
-      url('../assets/fonts/NotoSansKR-Bold.woff') format('woff'),
-      url('../assets/fonts/NotoSansKR-Bold.otf') format('truetype');
+    src: url(${NotoSans_KR_Bold_W2}) format('woff2'),
+      url(${NotoSans_KR_Bold_W}) format('woff'),
+      url(${NotoSans_KR_Bold_O}) format('truetype');
   }
   
   * { 
