5d281df5870bebf7fdaa97e895f5c2f62b237ea3
JZU0 <ljzh1030@gmail.com>
2022-11-19 02:18:23 +0900

    [#118] feat : SignUpCompleted 페이지 수정

--- src/pages/SignUpCompleted/index.tsx
+++ src/pages/SignUpCompleted/index.tsx
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom';
 
 import { Button, Typography } from '../../@components';
 
-import { Root, TextBox, Introduce, SignInButton } from './index.styles';
+import { Root, ButtonContainer, TextBox, Introduce } from './index.styles';
 import { btn1, color, font } from '../../styles/theme';
 
-const Onboarding = () => {
+const SignUpCompleted = () => {
   return (
     <Root>
-      <img id="good" src="/good.png" />
+      <img id="good" src="/good.svg" />
       <Introduce>
-        <Typography color={color.black} fontSize={16}>
+        <Typography color={color.black} fontSize={font.size.title}>
           반갑습니다
         </Typography>
         <TextBox>
           <Typography
             color={color.purple}
-            fontSize={16}
-            style={{ fontWeight: 'bold' }}
+            fontSize={font.size.title}
+            fontWeight={font.weight.bold}
           >
             abc@gmail.com
           </Typography>
         </TextBox>
         <TextBox>
-          <Typography color={color.black} fontSize={16}>
+          <Typography color={color.black} fontSize={font.size.title}>
             님!
           </Typography>
         </TextBox>
-        <Typography color={color.black} fontSize={16}>
+        <Typography color={color.black} fontSize={font.size.title}>
           JJAN 회원이 되신걸 축하드립니다!
         </Typography>
       </Introduce>
-      <img id="box" src="/signUpCompleted.png" />
-      <SignInButton>
+      <img id="box" src="/signUpCompleted.svg" />
+      <ButtonContainer>
         <Link to="/" style={{ textDecoration: 'none' }}>
           <Button style={btn1}>
             <Typography
@@ -45,9 +45,9 @@ const Onboarding = () => {
             </Typography>
           </Button>
         </Link>
-      </SignInButton>
+      </ButtonContainer>
     </Root>
   );
 };
 
-export default Onboarding;
+export default SignUpCompleted;
