3754fcbd7b138b00ddf008f16515a68abbf6c946
unsnruu <unsnruu@gmail.com>
2022-11-18 21:58:26 +0900

    [#112] feat : Layout 폴더의 위치를 수정

--- src/@components/Organism/Layout/index.tsx
+++ src/@components/Organism/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
-import Header from '../Header';
+import Header from '../../Molecule/Header';
 
 interface Props extends PropsWithChildren {
   headerTitle: string;
