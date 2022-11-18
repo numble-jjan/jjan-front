c66daae94762fdf900cb58caf900b647b76fbf48
unsnruu <unsnruu@gmail.com>
2022-11-18 22:06:45 +0900

    [#112] feat : color 스타일 정의를 theme 내부로 옮김

--- src/styles/constants/theme.ts
+++ src/styles/constants/theme.ts
@@ -1,4 +1,14 @@
-import color from './color';
+export const color = {
+  purple: '#683CED',
+  orange: '#F58229',
+  light_gray1: '#DDDDDD',
+  light_gray2: '#EEEEEE',
+  light_gray3: '#F9F9F9',
+  gray: '#666666',
+  dark_gray: '#999999',
+  black: '#000000',
+  white: '#FFFFFF',
+};
 
 export const btn1 = {
   backgroundColor: `${color.purple}`,
