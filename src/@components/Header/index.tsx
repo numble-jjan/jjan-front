fae26af30a9ee16c875df44a0c45e6d1ae5bfec7
unsnruu <unsnruu@gmail.com>
2022-11-18 17:14:12 +0900

    [#86] feat : 가독성을 향상시키기 위해 onClickPrev로 이름 변경

--- src/@components/Header/index.tsx
+++ src/@components/Header/index.tsx
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 import Typography from '../Typography';
 
 interface Props {
-  onClick: () => void;
+  onClickPrev: () => void;
   title: string;
 }
 
-const Header = ({ title, onClick }: Props) => {
+const Header = ({ title, onClickPrev }: Props) => {
   return (
     <Container>
-      <IconWrapper onClick={onClick}>
+      <IconWrapper onClick={onClickPrev}>
         <img src="icons/prev.svg" alt="이전으로 가기" />
       </IconWrapper>
       <Typography fontSize={18}>{title}</Typography>
