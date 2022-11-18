2e2893a321965c069db88e1ddd0593346e003b4f
JZU0 <ljzh1030@gmail.com>
2022-11-18 21:41:01 +0900

    [#93] feat : TitleBar 컴포넌트 수정

--- src/@components/TitleBar/index.tsx
+++ src/@components/TitleBar/index.tsx
@@ -1,40 +1,72 @@
 import React from 'react';
+import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
+import { Typography } from '@/@components';
+import color from '@/styles/constants/color';
+import { font } from '@/styles/constants/theme';
 
-type Props = {
-  children: string;
+interface Props extends PropsWithChildren {
   type?: 'default' | 'essential';
-};
+}
 
 const Root = styled.div`
-  width: 90%;
+  width: 100%;
   padding-bottom: 10px;
   border-bottom: 1px solid black;
 `;
 
 const Title = styled.div`
-  font-size: 16px;
-  font-weight: 500;
   display: inline-block;
 `;
 
 const Essential = styled.div`
-  font-size: 12px;
-  font-weight: 500;
   margin-top: 2px;
   display: inline-block;
   float: right;
 `;
 
-const TitleBar: React.FC<Props> = ({ children, type }) => {
+const TitleBar = ({ type, children }: Props) => {
   return type === 'essential' ? (
     <Root>
-      <Title>{children}</Title>
-      <Essential>필수사항</Essential>
+      <Title>
+        <Typography
+          color={color.black}
+          fontSize={font.size.title}
+          fontWeight={font.weight.medium}
+        >
+          {children}
+        </Typography>
+      </Title>
+      <Essential>
+        <Typography
+          color={color.black}
+          fontSize={font.size.s}
+          fontWeight={font.weight.medium}
+        >
+          필수사항
+        </Typography>
+      </Essential>
+      <Essential>
+        <Typography
+          color={color.purple}
+          fontSize={font.size.s}
+          fontWeight={font.weight.medium}
+        >
+          *
+        </Typography>
+      </Essential>
     </Root>
   ) : (
     <Root>
-      <Title>{children}</Title>
+      <Title>
+        <Typography
+          color={color.black}
+          fontSize={font.size.title}
+          fontWeight={font.weight.medium}
+        >
+          {children}
+        </Typography>
+      </Title>
     </Root>
   );
 };
