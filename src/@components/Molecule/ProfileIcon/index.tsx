f6c264fade2910cf26fe41edaf1a3b5d66be6806
unsnruu <unsnruu@gmail.com>
2022-11-20 16:55:04 +0900

    [#126] feat : ProfileIcon에 input[type="file"]을 설정

--- src/@components/Molecule/ProfileIcon/index.tsx
+++ src/@components/Molecule/ProfileIcon/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { color } from '@/styles/theme';
 
@@ -6,14 +6,27 @@ interface Props {
   extended?: boolean;
 }
 const ProfileIcon = ({ extended = false }: Props) => {
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (!hiddenFileInput.current) {
+      return;
+    }
+    hiddenFileInput.current.click();
+  };
   return (
     <IconContainer>
-      <img src="icons/icon-profile-anonymous.svg" alt="프로필 사진" />
+      <img
+        src="icons/icon-profile-anonymous.svg"
+        alt="프로필 사진"
+        onClick={handleClick}
+      />
       {extended && (
         <CameraCircle backgroundColor={color.purple}>
-          <img src="icons/icon-camera.svg" alt="카메라" />
+          <img src="icons/icon-camera.svg" alt="카메라 아이콘" />
         </CameraCircle>
       )}
+      <input type="file" ref={hiddenFileInput} />
     </IconContainer>
   );
 };
@@ -24,13 +37,16 @@ interface CameraCircleProps {
   backgroundColor: string;
 }
 
-const IconContainer = styled.div`
+const IconContainer = styled.form`
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 75px;
+  & input {
+    display: none;
+  }
 `;
 
 const CameraCircle = styled.div<CameraCircleProps>`
