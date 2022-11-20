dbf2511fd309066281b4712bbef18ccf4f25047d
Choi Hong Gyu <zxolqs@gmail.com>
2022-11-20 21:38:34 +0900

    [#144] feat : nickname schema 추가
    - 비동기 검증은 추가 및 정규표현식 추가 예정

--- src/constants/validationSchema.ts
+++ src/constants/validationSchema.ts
@@ -6,13 +6,21 @@ const EMAIL_VALIDATION_MESSAGE = {
 };
 
 const PASSWORD_VALIDATION_MESSAGE = {
-  requied: '비밀번호를 입력해주세요',
+  requied: '비밀번호를 입력해주세요.',
   notMatched: '비밀번호는 영문자, 숫자, 특수문자 조합을 입력해야 합니다.',
   notConfirmed: '비밀번호를 다시 확인해주세요.',
 };
 
+const NICKNAME_VALIDATION_MESSAGE = {
+  require: '닉네임을 입력해주세요.',
+  notMatched: '사용할 수 없는 닉네임입니다.',
+  existed: '이미 사용중인 닉네임입니다.',
+};
+
 const passwordRegex =
   /^(?=.*[A-Za-z])(?=.*d)(?=.*[$@$!%*#?&])[A-Za-zd$@$!%*#?&]{8,}$/;
+
+const nicknameRegex = /^/;
 
 export const schema = yup.object({
   email: yup
@@ -30,4 +38,8 @@ export const schema = yup.object({
       [yup.ref('password'), null],
       PASSWORD_VALIDATION_MESSAGE.notConfirmed,
     ),
+  nickname: yup
+    .string()
+    .required(NICKNAME_VALIDATION_MESSAGE.require)
+    .matches(nicknameRegex, NICKNAME_VALIDATION_MESSAGE.notMatched),
 });
