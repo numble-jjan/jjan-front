2b84d213db4c1333523684292862867e89f0200c
JZU0 <ljzh1030@gmail.com>
2022-11-23 02:47:37 +0900

    [#163] feat : 댓글 컴포넌트 제작

---
+++ src/@components/Molecule/Comment/index.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { ReplyComment } from '@/@components';
+import { Typography } from '@/@components';
+import { color, font } from '@/styles/theme';
+
+interface List {
+  id: number;
+  comment: string;
+  responseTo: string;
+}
+
+const Comment = () => {
+  const [id, setId] = useState<number>(0);
+  const [comment, setComment] = useState<string>('');
+  const [comments, setComments] = useState<List[]>([]);
+  const [isVaild, setIsVaild] = useState<boolean>(false);
+
+  const addComment = () => {
+    const data = {
+      id: id,
+      comment: comment,
+      responseTo: 'root',
+    };
+    setComments([...comments, data]);
+    setId(id + 1);
+    setComment('');
+    setIsVaild(false);
+  };
+
+  const handleOnKeyPress = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (e.key === 'Enter') {
+      addComment();
+    }
+  };
+
+  return (
+    <>
+      {comments.map((item: List) => {
+        return (
+          <div key={item.id}>
+            <Typography
+              color={color.black}
+              fontSize={font.size.m}
+              fontWeight={font.weight.medium}
+              style={{ lineHeight: '19px', letterSpacing: '-0.3px' }}
+            >
+              {item.comment}
+            </Typography>
+            <ReplyComment parentId={item.id} />
+          </div>
+        );
+      })}
+      <form>
+        <input
+          value={comment}
+          type={'text'}
+          placeholder="댓글 달기"
+          onChange={e => {
+            e.target.value.length > 0 ? setIsVaild(true) : setIsVaild(false);
+            setComment(e.target.value);
+          }}
+        />
+        <button
+          onClick={addComment}
+          onKeyPress={handleOnKeyPress}
+          disabled={isVaild ? false : true}
+        >
+          게시
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Comment;
