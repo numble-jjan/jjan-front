71adb315a78bf2ba57ae55b456a2431e539ae14a
JZU0 <ljzh1030@gmail.com>
2022-11-23 02:49:28 +0900

    [#163] feat : 컴포넌트 수정

--- src/@components/index.tsx
+++ src/@components/index.tsx
@@ -22,5 +22,7 @@ export { default as SearchInput } from './Molecule/SearchInput';
 export { default as ValidationInput } from './Molecule/ValidationInput';
 export { default as InterestedBarBox } from './Molecule/InterestedBarBox';
 export { default as KeywordList } from './Molecule/KeywordList';
+export { default as Comment } from './Molecule/Comment';
+export { default as ReplyComment } from './Molecule/ReplyComment';
 
 export { default as Layout } from './Organism/Layout';
