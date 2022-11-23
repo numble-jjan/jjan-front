d0d94828da640de0fd3a86df926c0299ce3a60bd
unsnruu <unsnruu@gmail.com>
2022-11-23 18:45:52 +0900

    [#166] feat : 게시글을 쓰는 페이지 추가
    게시글을 쓰는 'writepost' 페이지를 추가하였습니다.
    이후 라우트 구조의 변화에 따라 주소는 변할 수 있습니다.

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Onboarding from './pages/OnBoarding';
 import ProfileSetting from './pages/ProfileSetting';
 import SignUP from './pages/SignUp';
 import SignUpCompleted from './pages/SignUpCompleted';
 import Login from './pages/Login';
+import WritePost from './pages/WritePost';
 
 const AppRouter = () => {
   return (
@@ -16,6 +18,7 @@ const AppRouter = () => {
           <Route path="/signUp" element={<SignUP />} />
           <Route path="/profilesetting" element={<ProfileSetting />} />
           <Route path="/login" element={<Login />} />
+          <Route path="writepost" element={<WritePost />} />
         </Routes>
       </BrowserRouter>
     </>
