257a6247c23646caa9027d94e190ffcd2733d8fe
unsnruu <unsnruu@gmail.com>
2022-11-23 17:46:14 +0900

    [#160] feat : Image에 사용할 style을 생성 및 적용

--- src/@components/Atoms/Image/index.tsx
+++ src/@components/Atoms/Image/index.tsx
@@ -4,9 +4,10 @@ interface Props {
   src: string;
   alt: string;
   style: CSSProperties;
+  onClick?: () => void;
 }
 
-const Image = ({ src, alt, style }: Props) => {
-  return <img src={src} alt={alt} style={style} />;
+const Image = ({ src, alt, style, onClick }: Props) => {
+  return <img src={src} alt={alt} style={style} onClick={onClick} />;
 };
 export default Image;
