ec86cb5cf7f05f0b20da92f2b5a5c529db9fac12
unsnruu <unsnruu@gmail.com>
2022-11-24 16:13:10 +0900

    [#166] style : Badge 컴포넌트에 사용할 theme을 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -137,3 +137,29 @@ export const img: Img = {
     padding: '6px',
   },
 };
+
+type Badge = {
+  gray: CSSProperties;
+  purple: CSSProperties;
+  gray_font_black: CSSProperties;
+};
+export const badge: Badge = {
+  gray: {
+    border: `1px solid ${color.light_gray1}`,
+    color: `${color.light_gray1}`,
+    fontSize: `${font.size.s}`,
+    fontWeight: `${font.weight.regular}`,
+  },
+  purple: {
+    backgroundColor: `${color.purple}`,
+    color: `${color.white}`,
+    fontSize: `${font.size.s}`,
+    fontWeight: `${font.weight.regular}`,
+  },
+  gray_font_black: {
+    border: `1px solid ${color.light_gray1}`,
+    color: `${color.black}`,
+    fontSize: `${font.size.s}`,
+    fontWeight: `${font.weight.regular}`,
+  },
+};
