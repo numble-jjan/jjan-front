05aa6ccf1a3bf80e8960aa39048890c51f33da3b
unsnruu <unsnruu@gmail.com>
2022-11-24 09:17:56 +0900

    [#166] feat : ImageUploader 컴포넌트 생성

---
+++ src/@components/Molecule/ImageUploader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ImageUploader = () => {
+  return (
+    <Container>
+      <Form></Form>
+    </Container>
+  );
+};
+export default ImageUploader;
+
+const Container = styled.div``;
+const Form = styled.form``;
