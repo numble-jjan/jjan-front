a1e620f8503c1d83b2100fe1bdbfc41abb814512
unsnruu <unsnruu@gmail.com>
2022-11-24 10:39:25 +0900

    [#166] feat : UploadImageIcon 컴포넌트 구현

---
+++ src/@components/Molecule/UploadImageIcon/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from '@/@components/Atoms/Image';
+import { color } from '@/styles/theme';
+
+interface Props {
+  onClick?: () => void;
+}
+const UploadImageIcon = ({ onClick }: Props) => {
+  return (
+    <Container onClick={onClick}>
+      <Image src="icons/icon-camera-gray.svg" alt="카메라 아이콘" />
+    </Container>
+  );
+};
+export default UploadImageIcon;
+
+const Container = styled.div`
+  width: 74px;
+  height: 74px;
+  border: 1px solid ${color.light_gray1};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
