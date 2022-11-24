1e3d1a77cf7f2f7f6e26a1e0662ff01f31f6707f
unsnruu <unsnruu@gmail.com>
2022-11-24 09:08:25 +0900

    [#166] feat : PostTextarea 컴포넌트 생성
    Post를 글쓰기할 때 사용할 PostTextarea 컴포넌트를 생성하였습니다.

--- src/@components/Molecule/PostTextarea/index.tsx
+++ src/@components/Molecule/PostTextarea/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import Textarea from '@/@components/Atoms/Textarea';
+import { color, font } from '@/styles/theme';
 
-const PostTextarea = () => {
-  return <Textarea />;
+interface Props {
+  onChange?: () => void;
+}
+
+const PostTextarea = ({ onChange }: Props) => {
+  return (
+    <Container>
+      <Textarea onChange={onChange} />
+    </Container>
+  );
 };
 export default PostTextarea;
+
+const Container = styled.div`
+  position: relative;
+  & textarea {
+    width: 100%;
+    height: 200px;
+    border: 1px solid ${color.light_gray1};
+    font-size: ${font.size.m};
+    font-weight: ${font.weight.regular};
+  }
+`;
