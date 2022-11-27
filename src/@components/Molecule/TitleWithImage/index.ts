63f5cc8821e5c1f67e224a466e0b980492d20e2d
unsnruu <unsnruu@gmail.com>
2022-11-28 00:30:00 +0900

    [#182] feat : 내부 컴포넌트들을 Object.assign에 결합

--- src/@components/Molecule/TitleWithImage/index.ts
+++ src/@components/Molecule/TitleWithImage/index.ts
@@ -1,7 +1,13 @@
+import Main from './Main';
 import Image from './Image';
 import Title from './Title';
 import SubTitle from './SubTitle';
 import Emphasis from './Emphasis';
 
-const TitleWithImage = Object.assign();
+const TitleWithImage = Object.assign(Main, {
+  Image,
+  Title,
+  SubTitle,
+  Emphasis,
+});
 export default TitleWithImage;
