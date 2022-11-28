b6658233735da95a62ba9bade4816496ff92354a
JZU0 <ljzh1030@gmail.com>
2022-11-28 20:15:58 +0900

    [#181] feat : 메뉴바 아이콘 컴포넌트 제작

--- src/@components/Molecule/MenuBar/index.tsx
+++ src/@components/Molecule/MenuBar/index.tsx
@@ -1,61 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './index.styles';
-import { Typography } from '@/@components';
+import { Typography, MenuBarIcon } from '@/@components';
 import { color, font } from '@/styles/theme';
 
 // 페이지 이동 이벤트는 나중에 추가
 
 const MenuBar = () => {
+  const [currentPage, SetCurrentPage] = useState<string>('홈');
+  SetCurrentPage;
   return (
     <Styled.Root>
       <Styled.IconsContainer>
-        <Styled.IconContainer>
-          <img src="icons/icon-profile.svg" />
-          <Typography
-            color={color.black}
-            fontSize={font.size.menubar}
-            fontWeight={font.weight.medium}
-          >
-            프로필
-          </Typography>
-        </Styled.IconContainer>
-        <Styled.IconContainer>
-          <img src="icons/icon-location.svg" />
-          <Typography
-            color={color.black}
-            fontSize={font.size.menubar}
-            fontWeight={font.weight.medium}
-          >
-            추천술집
-          </Typography>
-        </Styled.IconContainer>
-        <Styled.IconContainer>
-          <img src="icons/icon-home.svg" />
-          <Typography
-            color={color.black}
-            fontSize={font.size.menubar}
-            fontWeight={font.weight.medium}
-          >
-            홈
-          </Typography>
-        </Styled.IconContainer>
-        <Styled.IconContainer>
-          <img src="icons/icon-information.svg" />
-          <Typography
-            color={color.black}
-            fontSize={font.size.menubar}
-            fontWeight={font.weight.medium}
-          >
-            술정보
-          </Typography>
-        </Styled.IconContainer>
+        <MenuBarIcon
+          src={
+            currentPage === '프로필'
+              ? 'icons/icon-selected-profile.svg'
+              : 'icons/icon-profile.svg'
+          }
+          menuTitle={'프로필'}
+          isSelected={currentPage === '프로필'}
+        />
+        <MenuBarIcon
+          src={
+            currentPage === '추천술집'
+              ? 'icons/icon-selected-location.svg'
+              : 'icons/icon-location.svg'
+          }
+          menuTitle={'추천술집'}
+          isSelected={currentPage === '추천술집'}
+        />
+        <MenuBarIcon
+          src={
+            currentPage === '홈'
+              ? 'icons/icon-selected-home.svg'
+              : 'icons/icon-home.svg'
+          }
+          menuTitle={'홈'}
+          isSelected={currentPage === '홈'}
+        />
+        <MenuBarIcon
+          src={
+            currentPage === '술정보'
+              ? 'icons/icon-selected-information.svg'
+              : 'icons/icon-information.svg'
+          }
+          menuTitle={'술정보'}
+          isSelected={currentPage === '술정보'}
+        />
       </Styled.IconsContainer>
       <Styled.CommunityIconWrapper>
-        <img src="icons/icon-community.svg" />
+        <img
+          src={
+            currentPage === '커뮤니티'
+              ? 'icons/icon-selected-community.svg'
+              : 'icons/icon-community.svg'
+          }
+        />
         <Typography
           color={color.white}
           fontSize={font.size.menubar}
-          fontWeight={font.weight.regular}
+          fontWeight={
+            currentPage === '커뮤니티' ? font.weight.bold : font.weight.regular
+          }
         >
           커뮤니티
         </Typography>
