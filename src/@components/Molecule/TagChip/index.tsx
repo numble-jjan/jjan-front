ed43d5c6e440ca7e1f86f80b4374be1fcd108421
unsnruu <unsnruu@gmail.com>
2022-11-29 13:24:53 +0900

    [#185] feat : TagChip 컴포넌트 구현

---
+++ src/@components/Molecule/TagChip/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Chip, Typography, Image } from '@/@components';
+import { badge, img } from '@/styles/theme';
+
+interface Props {
+  text: string;
+  onClickDelete?: () => void;
+  expanded?: boolean;
+}
+
+const TagChip = ({ text, expanded, onClickDelete }: Props) => {
+  return (
+    <Container style={badge.gray}>
+      <Typography>#{text}</Typography>
+      {expanded && (
+        <Image
+          src="icons/icon-cancel.svg"
+          alt="취소"
+          style={img.innerTagCircle}
+          onClick={onClickDelete}
+        />
+      )}
+    </Container>
+  );
+};
+export default TagChip;
+
+const Container = styled(Chip)`
+  & > img {
+    margin-left: 4px;
+  }
+`;
