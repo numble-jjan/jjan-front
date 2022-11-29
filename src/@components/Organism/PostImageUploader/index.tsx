7e33a7227a8559bd6644324a20e4432d308bd553
unsnruu <unsnruu@gmail.com>
2022-11-29 15:36:24 +0900

    [#185] feat : 이미지 업로드에 대한 기능 구현

--- src/@components/Organism/PostImageUploader/index.tsx
+++ src/@components/Organism/PostImageUploader/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { ImageUploader, Typography, UploadImageIcon } from '@/@components/';
 
+const DEFAULT_SRC = 'icons/icon-camera-gray.svg';
+
 const PostImageUploader = () => {
+  const [src, setSrc] = useState(DEFAULT_SRC);
+  const [uploaded, setUploaded] = useState(false);
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.currentTarget.files;
+    if (!files) return;
+
+    const file = files[0];
+    const newSrc = URL.createObjectURL(file);
+    setSrc(newSrc);
+    setUploaded(true);
+  };
+  const onClickDelete = () => {
+    setSrc(DEFAULT_SRC);
+    setUploaded(false);
+  };
+
   return (
     <Container>
       <Typography>이미지 첨부(선택)</Typography>
-      <ImageUploader ImageElement={<UploadImageIcon />} />
+      <ImageUploader
+        onChangeFile={onChangeFile}
+        uploaded={uploaded}
+        ImageElement={
+          <UploadImageIcon
+            src={src}
+            uploaded={uploaded}
+            onClickDelete={onClickDelete}
+          />
+        }
+      />
     </Container>
   );
 };
